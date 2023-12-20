420739da0f5bb7f8f9e90f18a7c52e9a55c83a30
Davide Botti <davidebotti92@gmail.com>
2023-12-20 23:03:38 +0100

    Working prepared statement to insert a record

--- adapters/src/main/java/adapters/CassandraVetRepository.java
+++ adapters/src/main/java/adapters/CassandraVetRepository.java
@@ -1,5 +1,7 @@
 package adapters;
 
+import com.datastax.oss.driver.api.core.cql.PreparedStatement;
+import com.datastax.oss.driver.api.core.cql.Statement;
 import domain.VetRepository;
 import org.springframework.data.cassandra.core.cql.CqlTemplate;
 
@@ -17,10 +19,12 @@ public class CassandraVetRepository  implements VetRepository {
 
     @Override
     public void add(domain.Vet vet) {
-       cqlTemplate.execute("");
+        PreparedStatement preparedStatement = cqlTemplate.getSession().prepare("insert into vet (id, name, surname) values (?, ?, ?)");
+        Statement insertStatement = preparedStatement.bind(UUID.randomUUID().toString(), vet.name(), vet.surname());
+       cqlTemplate.execute(insertStatement);
     }
 
-    private static Vet toEntity(domain.Vet vet) {
+    private Vet toEntity(domain.Vet vet) {
         return new Vet(UUID.randomUUID().toString(), vet.name(), vet.surname(), vet.specialties().stream().toList());
     }
 }
