1843b02256eeb90f903879dc983eb15338002063
Davide Botti <davidebotti92@gmail.com>
2023-12-21 18:06:40 +0100

    Adds the script to initialize the cassandra db every new launch

--- run-local-environment.sh
+++ run-local-environment.sh
@@ -1,5 +1,8 @@
 cd local-env
-docker compose up -d
-echo "Waiting for everything up..."
-sleep 2
+docker compose up -d cassandra
+echo "Waiting for cassandra to be up and running..."
+sleep 60
+docker compose up -d cassandra-load-keyspace
+echo "Initializing cassandra db..."
 docker ps
+echo "Everything should be up and running now!"
